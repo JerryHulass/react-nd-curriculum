a442548e6d823dd6f2a53294046cee4c0dff96de
steven <shotta3xd@gmail.com>
2025-04-14 15:02:24 -0400

    added solution for exercise

---
+++ break-countinue.js
@@ -0,0 +1,17 @@
+let arr = [1, 2, 3];
+
+for (let i = 0; i <= arr.length - 1; i++) {
+  if (arr[i] === 2) {
+    return;
+  }
+  console.log(arr[i]);
+}
+
+/**
+ *
+ * break -
+ *
+ * continue -
+ *
+ * return -
+ */
